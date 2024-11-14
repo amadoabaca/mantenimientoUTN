df80922224f59978149597684ddd585be4f1b063
Amado <amadoabaca99@gmail.com>
2024-11-14 00:31:54 -0300

    gestion activos

--- frontend/src/app/components/gestion/gestion-activos/gestion-activos.component.ts
+++ frontend/src/app/components/gestion/gestion-activos/gestion-activos.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-gestion-activos',
   standalone: true,
   imports: [],
   templateUrl: './gestion-activos.component.html',
-  styleUrl: './gestion-activos.component.css'
+  styleUrl: './gestion-activos.component.css',
 })
 export class GestionActivosComponent {
+  constructor(private router: Router) {}
 
+  goBack() {
+    this.router.navigate(['/gestion']);
+  }
 }
